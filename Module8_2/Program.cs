86fb1a932f32147188c5d640ba7ad4d5b2d994d7
Kramerushka <email@example.rupainandlove95@gmail.com>
2024-01-11 17:21:39 +0600

    Задание 8.4.3

---
+++ Module8_2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Module8
+{
+    /// <summary>
+    /// Задание 8.4.3
+    /// </summary>
+    /// Дан класс. Доработайте его и сериализуйте в бинарный формат
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contact person = new Contact("Vik", 77052439520, "painandlove95@mail.ru");
+            Console.WriteLine("Объект создан");
+            Console.WriteLine();
+
+            BinaryFormatter bf = new BinaryFormatter();
+            using (FileStream fs = new FileStream("myContacts.dat", FileMode.OpenOrCreate))
+            {
+                bf.Serialize(fs, person);
+                Console.WriteLine("Объект сериализован");
+                Console.WriteLine();
+            }
+        }
+    }
+
+    [Serializable]
+    class Contact
+    {
+        public string Name { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public Contact(string name, long phoneNumber, string email)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+    }
+}
