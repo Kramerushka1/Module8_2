5143431abf1687cc254852a1b602fe9809e5be98
Виктор Кононович <mr.doomka@mail.ru>
2024-01-18 11:24:52 +0600

    Задание 8.2.4

--- Module8_2/Program.cs
+++ Module8_2/Program.cs
@@ -1,45 +1,130 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
 using System.IO;
-using System.Linq;
-using System.Reflection.PortableExecutable;
-using System.Runtime.Serialization.Formatters.Binary;
+using static System.Net.WebRequestMethods;
 
 namespace Module8
 {
-    /// <summary>
-    /// Задание 8.4.3
-    /// </summary>
-    /// Дан класс. Доработайте его и сериализуйте в бинарный формат
     class Program
     {
         static void Main(string[] args)
         {
-            Contact person = new Contact("Vik", 77052439520, "painandlove95@mail.ru");
-            Console.WriteLine("Объект создан");
-            Console.WriteLine();
+            string desktop = @"C:\Users\mrdoo\desktop";
+            string newDir = @"C:\Users\mrdoo\desktop\testFolder";
 
-            BinaryFormatter bf = new BinaryFormatter();
-            using (FileStream fs = new FileStream("myContacts.dat", FileMode.OpenOrCreate))
+            DirAndFilesCount(desktop);
+
+            DirCreate(newDir);
+            DirAndFilesCount(desktop);
+
+            MoveToRecycleBin(newDir);
+            DirAndFilesCount(desktop);
+
+        }
+        static void DrivesInformation()
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drive in drives)
             {
-                bf.Serialize(fs, person);
-                Console.WriteLine("Объект сериализован");
+                Console.WriteLine($"Название: {drive.Name}");
+                Console.WriteLine($"Тип: {drive.DriveType}");
+                if (drive.IsReady)
+                {
+                    Console.WriteLine($"Объем: {drive.TotalSize}");
+                    Console.WriteLine($"Осталось: {drive.TotalFreeSpace}");
+                    Console.WriteLine($"Метка: {drive.VolumeLabel}");
+                }
                 Console.WriteLine();
             }
         }
-    }
-
-    [Serializable]
-    class Contact
-    {
-        public string Name { get; set; }
-        public long PhoneNumber { get; set; }
-        public string Email { get; set; }
-
-        public Contact(string name, long phoneNumber, string email)
+        static void GetCatalogs()
         {
-            Name = name;
-            PhoneNumber = phoneNumber;
-            Email = email;
+            string dirName = @"C:\";
+
+            if (Directory.Exists(dirName))
+            {
+                Console.WriteLine("Папки:");
+                string[] dirs = Directory.GetDirectories(dirName);
+                foreach (string dir in dirs)
+                {
+                    Console.WriteLine(dir);
+                }
+                Console.WriteLine();
+
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(dirName);
+                foreach (string file in files)
+                {
+                    Console.WriteLine(file);
+                }
+            }
+        }
+        static void DirAndFilesCount(string dir)
+        {
+            try
+            {
+                if (Directory.Exists(dir))
+                {
+                    string[] dirs = Directory.GetDirectories(dir);
+                    string[] files = Directory.GetFiles(dir);
+                    Console.WriteLine($"Всего папок и файлов: {files.Length + dirs.Length}");
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static void DirCreate(string dir)
+        {
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(dir);
+                if (!dirInfo.Exists)
+                {
+                    dirInfo.Create();
+                    Console.WriteLine("Папка testFolder создана");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        static void DirDelete()
+        {
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(@"C:\New Folder");
+
+                if (dirInfo.Exists)
+                {
+                    dirInfo.Delete();
+                    Console.WriteLine("Папка удалена");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        static void MoveToRecycleBin(string dirDelete)
+        {
+            try
+            {
+                FileSystem.DeleteDirectory(
+                    dirDelete,
+                    UIOption.AllDialogs,
+                    RecycleOption.SendToRecycleBin,
+                    UICancelOption.ThrowException);
+                Console.WriteLine("Папка testFolder удалена в корзину.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при удалении папки в корзину: {ex.Message}");
+            }
         }
     }
 }
